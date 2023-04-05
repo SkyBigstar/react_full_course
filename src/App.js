7ce122fcd55b9597ad3f14abb21a21044ddba18d
SkyBigstar <SkyBigstar1990@gmail.com>
2023-04-06 02:13:33 +0900

    movieland

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import searchIcon from './search.svg'
+import MovieCard from './MovieCard';
 
-function App() {
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=79513750'
+
+const App = () => {
+ const [moviecontent, setMoviecontent] = useState([]);
+ const [toogleItem, setToogleItem] = useState('');
+ const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+    setMoviecontent(data.Search);
+ }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input placeholder='Search for Movies' value={toogleItem} onChange={(e)=> setToogleItem(e.target.value)} />
+        <img src={searchIcon} alt="search" onClick={() => searchMovies(toogleItem)}/>
+      </div>
+
+      {
+        moviecontent?.length > 0
+          ? (
+            <div className='container'>
+              {moviecontent.map((movie) => (
+                <MovieCard movie={movie}/>
+              ))}
+             
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
     </div>
   );
 }
